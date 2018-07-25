5ccf88e0d816829d5e45013da6e1ba31fbe47a50
Mayoogh Girish <mayoogh1997@gmail.com>
2018-07-25 17:53:48 +0530

    updated android studio source code

---
+++ LED-master/app/src/main/java/com/led_on_off/led/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.led_on_off.led;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by PEACE on 3/30/2016.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(700);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,DeviceList.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
