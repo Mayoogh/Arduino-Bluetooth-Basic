8a45f63a165666dbdb1c77b29b67d3427ea55b19
Mayoogh Girish <mayoogh1997@gmail.com>
2019-01-08 16:10:20 +0530

    Changed variable data to Incoming_value
    data was reserved keyword.

--- Arduino Code/Arduino_Bluetooth_Basic.ino
+++ Arduino Code/Arduino_Bluetooth_Basic.ino
@@ -5,7 +5,7 @@
  *  Download the App : 
  *  This program lets you to control a LED on pin 13 of arduino using a bluetooth module
  */
-char data = 0;                //Variable for storing received data
+char Incoming_value = 0;                //Variable for storing Incoming_value
 void setup() 
 {
   Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
@@ -13,14 +13,14 @@ void setup()
 }
 void loop()
 {
-  if(Serial.available() > 0)  // Send data only when you receive data:
+  if(Serial.available() > 0)  
   {
-    data = Serial.read();      //Read the incoming data and store it into variable data
-    Serial.print(data);        //Print Value inside data in Serial monitor
+    Incoming_value = Serial.read();      //Read the incoming data and store it into variable Incoming_value
+    Serial.print(Incoming_value);        //Print Value of Incoming_value in Serial monitor
     Serial.print("\n");        //New line 
-    if(data == '1')            //Checks whether value of data is equal to 1 
+    if(Incoming_value == '1')            //Checks whether value of Incoming_value is equal to 1 
       digitalWrite(13, HIGH);  //If value is 1 then LED turns ON
-    else if(data == '0')       //Checks whether value of data is equal to 0
+    else if(Incoming_value == '0')       //Checks whether value of Incoming_value is equal to 0
       digitalWrite(13, LOW);   //If value is 0 then LED turns OFF
   }                            
  
