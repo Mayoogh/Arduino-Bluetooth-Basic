67dc05b271e27785722c711222bcde058a12c497
Sena W <62844586+cswsen2@users.noreply.github.com>
2020-04-26 20:44:36 +0530

    Update Arduino_Bluetooth_Basic.ino

--- Arduino Code/Arduino_Bluetooth_Basic.ino
+++ Arduino Code/Arduino_Bluetooth_Basic.ino
@@ -16,8 +16,8 @@ void loop()
   if(Serial.available() > 0)  
   {
     Incoming_value = Serial.read();      //Read the incoming data and store it into variable Incoming_value
-    Serial.print(Incoming_value);        //Print Value of Incoming_value in Serial monitor
-    Serial.print("\n");        //New line 
+   
+    Serial.println();        //New line 
     if(Incoming_value == '1')            //Checks whether value of Incoming_value is equal to 1 
       digitalWrite(13, HIGH);  //If value is 1 then LED turns ON
     else if(Incoming_value == '0')       //Checks whether value of Incoming_value is equal to 0
